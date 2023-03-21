ea441b4452c5b466e1d66f4a577dff492bc44a1b
Gabin Laigle <gabin.laigle@worldline.com>
2023-03-21 19:16:11 +0100

    feat: change text to new theme

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,23 +5,22 @@
 <template>
     <div>
         <h1>Gabin Laigle</h1>
-        <!-- <h2>Relax, you found the right person</h2>  -->
-        <h2>Work in progress, checkout my <span class="link"><a href="https://www.linkedin.com/in/gabin-laigle/">LinkedIn</a></span> meanwhile</h2>
+        <h2>Relax, you found the right <span class="link"><a href="https://www.linkedin.com/in/gabin-laigle/">software engineer</a></span></h2>
     </div>
 </template>
 
 <style scoped>
 
 div {
-    @apply flex flex-col h-screen justify-center items-center
+    @apply flex flex-col h-screen justify-center items-start px-48
 }
 
 h1 {
-    @apply text-center text-5xl md:text-6xl xl:text-7xl px-12
+    @apply text-5xl md:text-6xl xl:text-7xl
 }
 
 h2 {
-    @apply text-center text-3xl md:text-4xl xl:text-5xl font-light mt-8 px-12
+    @apply text-2xl md:text-3xl xl:text-4xl font-light mt-8
 }
 
 .link {
