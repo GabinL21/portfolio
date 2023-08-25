96965ba70fc80acce2ad5efaaeb49834cf912d17
gabin_laigle <gabin.laigle21@gmail.com>
2023-08-25 12:34:18 +0200

    feat(values): add value items

---
+++ src/components/ValueItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps(["title", "content"]);
+</script>
+
+
+<template>
+    <div class="wrapper">
+        <h2 class="title">{{ title }}</h2>
+        <div class="separator"></div>
+        <p class="content">{{ content }}</p>
+    </div>
+</template>
+
+<style scoped>
+.wrapper {
+    @apply flex flex-col items-center space-y-2 md:space-y-8;
+}
+
+.title {
+    @apply font-semibold text-3xl md:text-5xl text-center;
+}
+
+.content {
+    @apply font-normal text-lg md:text-3xl text-center;
+}
+
+.separator {
+    @apply h-1 rounded-full bg-accent-500 w-5/6;
+}
+</style>
--- src/components/ValuePage.vue
+++ src/components/ValuePage.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
+import ValueItem from './ValueItem.vue';
 
 onMounted(() => {
     const logoPart1 = document.querySelector(".part1");
     const logoPart2 = document.querySelector(".part2");
     const logoPart3 = document.querySelector(".part3");
-
     const logoParts = [logoPart1, logoPart2, logoPart3];
+
+    const value1 = document.querySelector(".value1");
+    const value2 = document.querySelector(".value2");
+    const value3 = document.querySelector(".value3");
+    const valueItems = [value1, value2, value3];
 
     function selectPart(activeLogoPart: Element) {
         logoParts.forEach(logoPart => logoPart?.classList.remove("active-part"));
         activeLogoPart.classList.add("active-part");
         activeLogoPart.classList.remove("pulse-part");
+        let index = logoParts.indexOf(activeLogoPart);
+        valueItems.forEach(valueItem => valueItem?.classList.remove("active-value"));
+        valueItems[index]?.classList.add("active-value");
     }
 
-    logoParts.forEach(logoPart => logoPart?.addEventListener("mouseover", () => selectPart(logoPart)));
+    logoParts.forEach(logoPart => logoPart?.addEventListener("mouseenter", () => selectPart(logoPart)));
     logoParts.forEach(logoPart => logoPart?.addEventListener("touchstart", () => selectPart(logoPart)));
 });
 </script>
@@ -27,12 +35,17 @@ onMounted(() => {
             <img src="src/assets/logo/logo_part2.svg" alt="logo part2" class="part part2 pulse-part">
             <img src="src/assets/logo/logo_part3.svg" alt="logo part3" class="part part3 pulse-part">
         </div>
+        <div class="values-items">
+            <ValueItem class="value value1" title="Product-focused" content="As much as I love software it is just a tool, the need is at the center of everything." />
+            <ValueItem class="value value2" title="Product-focused2" content="As much as I love software it is just a tool, the need is at the center of everything." />
+            <ValueItem class="value value3" title="Product-focused3" content="As much as I love software it is just a tool, the need is at the center of everything." />
+        </div>
     </section>
 </template>
 
 <style scoped>
 .values-wrapper {
-    @apply flex flex-col h-screen justify-center items-center snap-start space-y-32 md:space-y-64
+    @apply flex flex-col h-screen justify-center items-center snap-start space-y-32 md:space-y-48
 }
 
 .values-logo {
@@ -52,7 +65,19 @@ onMounted(() => {
     filter: invert(45%) sepia(28%) saturate(6471%) hue-rotate(347deg) brightness(97%) contrast(96%);
 }
 
+.values-items {
+    @apply flex items-center w-2/3 md:w-1/3;
+}
+
 h1 {
     @apply text-6xl md:text-7xl xl:text-8xl xl:text-start text-center font-playfair-display mt-16
+}
+
+.value {
+    @apply hidden;
+}
+
+.value.active-value {
+    @apply flex;
 }
 </style>
