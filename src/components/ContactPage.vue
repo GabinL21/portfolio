cbace8130643c6328c9012a825e9f77a3ff79f74
gabin_laigle <gabin.laigle21@gmail.com>
2023-08-26 01:38:26 +0200

    feat(contact): add social icons

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -8,7 +8,12 @@
             <h1>Contact Me</h1>
         </div>
         <div class="footer">
-            <a href="https://www.termsfeed.com/live/9cfe31d3-97a8-4c26-a0f8-2bc583f12420">Terms & Conditions</a>
+            <div class="icons">
+                <a href="mailto:gabin.laigle21@gmail.com" target="_blank"><font-awesome-icon :icon="['far', 'envelope']" class="icon"/></a>
+                <a href="https://www.linkedin.com/in/gabin-laigle/" target="_blank"><font-awesome-icon :icon="['fab', 'linkedin-in']" class="icon" /></a>
+                <a href="https://github.com/GabinL21/doctobot" target="_blank"><font-awesome-icon :icon="['fab', 'github']" class="icon" /></a>
+            </div>
+            <a href="https://www.termsfeed.com/live/9cfe31d3-97a8-4c26-a0f8-2bc583f12420" class="terms">Terms & Conditions</a>
         </div>
     </section>
 </template>
@@ -19,17 +24,23 @@
 }
 
 .content {
-    height: 90vh;
-    @apply flex flex-col items-center justify-center;
+    @apply h-4/5 flex flex-col items-center justify-center;
 }
 
 .footer {
-    height: 10vh;
-    @apply flex flex-col items-center justify-center;
+    @apply h-1/5 flex flex-col items-center justify-center space-y-4;
 }
 
-a {
-    @apply hover:text-accent-500 text-sm md:text-base;
+.icons {
+    @apply flex flex-row space-x-8;
+}
+
+.icon {
+    @apply text-secondary hover:text-accent-700 h-7 transition-all duration-200;
+}
+
+.terms {
+    @apply hover:text-accent-500 text-sm md:text-base transition-all duration-200;
 }
 
 h1 {
